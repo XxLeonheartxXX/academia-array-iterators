3fa3bc2278f23f19726f9f0fb6431a31553f7fb4
XxLeonheartxXX <saul.delarosam@gmail.com>
2019-05-20 08:27:09 -0700

    fork modificado

--- workHere.js
+++ workHere.js
@@ -4,7 +4,20 @@ var billionaires = require('./billionaires.json');
     Get the names of the billionaires that are in their 20s
 */
 
-let twentyYearOldBillionaires = null;
+
+
+
+
+let twentyYearOldBillionaires = billionaires.filter(x => {
+        if (x.demographics.age >= 20 && x.demographics.age < 30)
+        {
+            return x.name;
+        }
+    });
+
+
+
+
 
 /*
     create a new object with the billionairse year, name and worth rounded to the newarest integer
@@ -15,32 +28,52 @@ let twentyYearOldBillionaires = null;
     }
 */
 
-let roundedWorth = null;
+let roundedWorth = billionaires.map(x => {
+        return{
+            year: x.year,
+            name: x.name,
+            worth: Math.round(x.wealth['worth in billions'])
+        }
+    });
+    
+
 
 /*
     sum the rounded worth of all the billionaires
 */
 
-let aggregatedWorth = null;
-// console.log(aggregatedWorth)
+let aggregatedWorth = 
+billionaires.reduce((prev, curr)=>{
+    
+  return prev + Math.round(curr.wealth['worth in billions']);
+}, 0);
+ //console.log(aggregatedWorth);
 
 /*
     find the the richest billionaire, make sure the result is in an array
 */
 
-let richest = null
+let richest = billionaires.filter(x=>{
+    if(x.rank == 1){
+        x.name;
+    }
+});
 
 /*
     find the poorest billioanire, make sure the result is in an array
 */
 
-let poorest = null;
+let poorest = billionaires.filter(x=>{
+    if(x.rank !== null){
+        ;
+    }
+});
 
 /*
     make a new array containing the richest and the poorest billionaire
 */
 
-let richAndPoor = null;
+let richAndPoor = console.log("pobres " + poorest() +  " ricos" richest());
 
 module.exports = {
     twentyYearOldBillionaires,
